4a49dce7ba3ad2fef84215b5d82f1308be835236
chips <christopher.e.yarbrough@gmail.com>
2018-05-08 14:57:24 -0600

    Added bones

---
+++ cfg.py
@@ -0,0 +1,14 @@
+#cfg.py
+#Bot configurations
+#Version 1.0
+
+
+HOST = "irc.twitch.tv"
+PORT = 6667
+NICK = "PunchBot"
+PASS = "oauth:9mqosbohszi0v5sc1ayommfcsx8ub6"
+CHAN = "punchpunchtest"
+RATE = (20/30) # msgs per sec
+
+oplist = {}
+
---
+++ punchbot.py
@@ -0,0 +1,43 @@
+# punchbot.py
+# Main code for bot
+
+import cfg
+import utils
+import socket
+import re
+import time
+import thread
+from time import sleep
+
+deaths = 0
+
+def main():
+    # Networking
+    s = socket.socket()
+    s.connect((cfg.HOST, cfg.PORT))
+    s.send("PASS {}\r\n".format(cfg.PASS).encode("utf-8"))
+    s.send("NICK {}\r\n".format(cfg.NICK).encode("utf-8"))
+    s.send("JOIN {}\r\n".format(cfg.CHAN).encode("utf-8"))
+
+    CHAT_MSG = re.compile(r"^:\w+!w+@w+.tmi\.twitch\.tv PRIMSG #\w+ :")
+    utils.chat(s, "What up party people!")
+
+    thread.start_new_thread(utils.threadFillOpList, ())
+
+    while True:
+        response = s.recv(1024).decode("utf-8")
+        if reponse == "PING :tmi.tiwtch.tv\r\n":
+            s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
+        else:
+            username = re.search(r"\w+", response).group(0)
+            message = CHAT_MSG.sub("", response)
+            print(response)
+
+            if message.strip() == "!dead":
+                deaths = deaths + 1
+                utils.chat(s, "Drew has died " + deaths + "times")
+        sleep(1)
+
+
+if __name__ == "__main__" :
+    main()
